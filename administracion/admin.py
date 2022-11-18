db7f13bc5f4952b41935b9a7e896bbcfae3ced34
Facher05MB <maxi05blazquez@gmail.com>
2022-11-18 12:03:44 -0300

    medio admin site

--- administracion/admin.py
+++ administracion/admin.py
@@ -2,13 +2,38 @@ from django.contrib import admin
 
 from .models import *
 
+class BusAdmin(admin.ModelAdmin):
+    list_filter = ("patente","numero_unidad")
+    search_fields = ['patente']
 
+class ChoferAdmin(admin.ModelAdmin):
+    list_filter = ("legajo","nombre","apellido")
+    search_fields = ['nombre','apellido']
+    
+    
+class DetalleCadaParadaAdmin(admin.ModelAdmin):
+    list_filter = ("numero_orden","conexion","parada")
+    search_fields = ['numero_orden']
+    
+    
+class ParadaAdmin(admin.ModelAdmin):
+    list_filter = ("nombre","direccion","atractivos")
+    search_fields = ['nombre']
+    
+    
+class RecorridoAdmin(admin.ModelAdmin):
+    list_filter = ("nombre","frecuencia",'duracion_aprox')
+    search_fields = ['nombre']
+    
+    
+class ViajeAdmin(admin.ModelAdmin):
+    list_filter = ("hora_inicio_prevista","numero","hora_fin")
+    search_fields = ['numero']
 
-
-admin.site.register(Bus)
-admin.site.register(Chofer)
+admin.site.register(Bus,BusAdmin)
+admin.site.register(Chofer,ChoferAdmin)
 admin.site.register(Atractivo)
 admin.site.register(DetalleCadaParada)
-admin.site.register(Parada)
-admin.site.register(Recorrido)
-admin.site.register(Viaje)
+admin.site.register(Parada,ParadaAdmin)
+admin.site.register(Recorrido,RecorridoAdmin)
+admin.site.register(Viaje,ViajeAdmin)
