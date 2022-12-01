dfbdc4084fa6aea5a0a20406b4e20b9c904326a8
Guillermo-maker <gustavoandino2@gmail.com>
2022-12-01 09:53:12 -0300

    views

--- administracion/views.py
+++ administracion/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-    
+from .models import *
 
 def lal(request):
    return render(request,'administracion/ejemplo.html')
+
+def login(request):
+    chofer = Chofer.objects.all()
+    return render(request, 'administracion/login.html',{'chofer': chofer})
