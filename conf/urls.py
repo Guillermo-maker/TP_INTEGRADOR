de5e198922504bb1d7345accf639a7d1e3218adb
Guillermo-maker <gustavoandino2@gmail.com>
2022-12-01 09:50:30 -0300

    URLS

--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from administracion.views import lal
+from administracion.views import lal,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first/', lal),
+    path('login/',login),
 ]
-
