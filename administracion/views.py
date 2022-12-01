367f9eec33a7baddc7e7b831e2837ddfb056d70f
Facher05MB <maxi05blazquez@gmail.com>
2022-12-01 11:18:33 -0300

    toma fercho

--- administracion/views.py
+++ administracion/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from .forms import ChoferForm
     
 
 def lal(request):
@@ -25,17 +26,16 @@ def login(request):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 def data_read_buses(request):
     context ={'data_read_chofer':Bus.objects.all()}
     return render (request, "data_read_chofer.html",context)
+
+def data_form_buses (request):
+    if request.method == "POST":
+        form = ChoferForm (request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect ('/leer_chofer')
+    else:
+        form = ChoferForm ()
+        return render (request, "data_from_chofer.html",{'form':form})  
--- conf/urls.py
+++ conf/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from administracion.views import lal,reporte,login
+from administracion.views import lal,reporte,login,data_read_buses,data_form_buses
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first/', lal),
     path('reporte/', reporte),
     path('login/', login),
+    path('agregar_chofer/', data_form_buses),
+    path('leer_chofer/', data_read_buses),
+    
 ]
 
