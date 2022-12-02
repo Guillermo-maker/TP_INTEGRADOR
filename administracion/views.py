f78fb94e0aa317493ec62fcfc50abeb5c229e9f7
Guillermo-maker <gustavoandino2@gmail.com>
2022-12-02 08:22:15 -0300

    preparando merge

--- administracion/views.py
+++ administracion/views.py
@@ -2,15 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
     
-
-def lal(request):
-   viajes = Viaje.objects.all()
-   
-   viajesAnalisis= Viaje.objects
-   
-   
-   return render(request,'administracion/ejemplo.html',{"viajes":viajes})
-
-def login(request):
-    chofer = Chofer.objects.all()
-    return render(request, 'administracion/login.html',{'chofer': chofer})
--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from administracion.views import lal,login
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first/', lal),
-    path('login/',login),
 ]
